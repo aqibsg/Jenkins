e4683c96e54015a8d94e520fde8d2f53b42802f3
Aqib Siraj <aqibsiraj@gmail.com>
2018-11-16 12:30:39 +0530

    Update asert.py

--- asert.py
+++ asert.py
@@ -1,10 +1,3 @@
 # assertion testing
 tt
-str = "hello world!"
-#commenthry
-,cdcdccc
-jmhj,yhj,kljkbhnjbnkjb
-8uikjkhvvcdwcdwcdwcdewc
-jjhbvv
-hnjkFVVFEVDFVFVVdfffvfvdfvfddddddddddddddddFVDVFVFVFVFVFVddd
-dddddd
+
